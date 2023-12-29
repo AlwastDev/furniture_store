6c0f573bc37a5b764adc10f9abdcee0331361a41
AlwastDev <sshapar13371@gmail.com>
2023-12-29 15:42:43 +0200

    added subheader

--- client/src/public/index.ts
+++ client/src/public/index.ts
@@ -4,5 +4,21 @@ import ArrowIcon from '@public/icons/arrowIcon.svg';
 import HeartIcon from '@public/icons/heartIcon.svg';
 import PersonIcon from '@public/icons/personIcon.svg';
 import CartIcon from '@public/icons/cartIcon.svg';
+import SearchIcon from '@public/icons/searchIcon.svg';
 
-export { MailIcon, PhoneIcon, ArrowIcon, HeartIcon, PersonIcon, CartIcon };
+import ChairImage from '@public/images/chairImage.png';
+import DiscountImage from '@public/images/discountImage.png';
+import lampImage from '@public/images/lampImage.png';
+
+export {
+  MailIcon,
+  PhoneIcon,
+  ArrowIcon,
+  HeartIcon,
+  PersonIcon,
+  CartIcon,
+  SearchIcon,
+  ChairImage,
+  DiscountImage,
+  lampImage,
+};
